7c8c015e2f4a0c2a0b82af0a510097e63c10ad64
KondoHiroshi <43530692+KondoHiroshi@users.noreply.github.com>
2020-05-29 18:17:16 +0900

    Update xffts_server.py

--- scripts/xffts_server.py
+++ scripts/xffts_server.py
@@ -1,5 +1,7 @@
 #! /usr/bin/env python
 
+import time
+import threading
 import xfftspy
 
 import rospy
@@ -11,19 +13,28 @@ def spectra_fowarding_loop(xffts):
     xffts.clear_buffer()
     while not rospy.is_shutdown():
         d = xffts.receive_once()
-
+        datatime= (time.time(),)
         for bnum in d['data']:
             if bnum not in pub_dict:
                 pub_dict[bnum] = rospy.Publisher(
                                         '/xffts_board{0:02d}'.format(bnum),
-                                        std_msgs.msg.Float32MultiArray,
+                                        std_msgs.msg.Float64MultiArray,
                                         queue_size = 1)
                 pass
-            spec = std_msgs.msg.Float32MultiArray()
-            spec.data = d['data'][bnum]
+            spec = std_msgs.msg.Float64MultiArray()
+            spec.data = d['data'][bnum]+datatime
             pub_dict[bnum].publish(spec)
+            #print(bnum)
             continue
         continue
+    return
+
+def sum(self, q, arg):
+    _tp = sum(q.data[:-1])
+    t = q.data[-1]
+    tp = Float64MultiArray()
+    tp.data = [_tp,t]
+    self.pub[arg].publish(tp)
     return
 
 if __name__ == '__main__':
@@ -31,4 +42,7 @@ if __name__ == '__main__':
     host = 'localhost'
 
     xffts = xfftspy.data_consumer(host)
-    spectra_fowarding_loop(xffts)
+    th = threading.Thread(target=spectra_fowarding_loop, args=(xffts,))
+    th.start()
+
+    rospy.spin()
